f5799753d8cc630baf88cf6640fe7f1ea354e0c1
bigonese <eric.bigoness@firelit.com>
2013-06-18 21:21:16 -0400

    Initial commit

---
+++ minify.php
@@ -0,0 +1,119 @@
+#!/usr/bin/php
+<?PHP
+
+require_once('vendor/autoload.php');
+
+// Remove script name
+array_shift($argv);
+
+$recursive = false;
+$targets = array();
+
+do {
+	
+	$arg = current($argv);
+	
+	if ($arg === false) // No more arguements
+		break;
+		
+	if ($arg == '-r') {
+		// Config file option
+		
+		$recursive = true;
+		
+	} else {
+		
+		$targets[] = $arg;
+		
+	}
+	
+} while (next($argv));
+
+if (!sizeof($targets))
+	errorExit('No target files specified.');
+	
+function walk($target, $recursive = false) {
+	if (is_dir($target)) {
+		
+		$d = opendir($target);
+		
+		while ($next = readdir($d)) {
+			
+			if (preg_match('/^\./', $next)) continue;
+			
+			if (is_dir($next) && $recursive) {
+				walk($next, $recursive);
+			} elseif (!is_dir($next)) {
+				minify($target . DIRECTORY_SEPARATOR . $next);
+			}
+			
+		}
+		
+		closedir($d);
+		
+	} else {
+		minify($target);
+	}
+}
+
+function minify($targetFile) {
+	
+	// Already minified
+	if (preg_match('/\.min\.(css|js)$/i', $targetFile)) return;
+	
+	// Doesn't exist?
+	if (!file_exists($targetFile)) return;
+	
+	// Can't read it?
+	if (!is_readable($targetFile)) 
+		errorExit('Target file not readable: '. $targetFile);
+	
+	$fileNameArray = explode('.', $targetFile);
+	$suffix = array_pop($fileNameArray);
+	array_push($fileNameArray, 'min'); // New name is *.min.js or *.min.css
+	array_push($fileNameArray, $suffix);
+	
+	$newFileName = implode('.', $fileNameArray);
+	
+	$targetFileContents = file_get_contents($targetFile);
+	
+	if (preg_match('/\.css$/i', $targetFile)) {
+		// CSS
+		
+		$minString = CssMin::minify($targetFileContents);
+				
+		$res = file_put_contents($newFileName, $minString);
+		
+	} elseif (preg_match('/\.js$/i', $targetFile)) {
+		// Javascript
+		
+		exec('java -jar vendor/google/compiler.jar --js '. $targetFile .' --js_output_file '. $newFileName, $out, $res);
+		
+	} else // Unknown file type
+		return; 
+		
+	if ($res === false) 
+		errorExit('Unable to write minified file: '. $newFileName, implode("\n", $out));
+	
+	$before = filesize($targetFile);
+	$after = filesize($newFileName);
+	
+	$change = round( ( $before - $after ) / $before, 2 ) * 100;
+	
+	writeLine($newFileName .' minified (size reduced by '. $change .'%)');
+	
+}
+
+// Change
+foreach ($targets as $target) 
+	walk($target, $recursive);
+	
+function errorExit($string, $errInfo = false) {
+	fwrite(STDERR, "ERROR: ". $string ."\n");
+	if ($errInfo) fwrite(STDERR, $errInfo ."\n");
+	exit(1);
+}
+
+function writeLine($string) {
+	fwrite(STDOUT, $string ."\n");
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+## Overview
+This is a quick command line CSS & JS minifier. Designed to be run during the build phase of a web app's deployment. It creates a second version of every .js and .css file (with the extension .min.js and .min.css). It's left up to the dev to decide how to reference these minified versions in production.
+
+It uses the [Closure Compilier](https://developers.google.com/closure/compiler/) (by Google) to minify the JavaScript. It uses the [CssMin PHP project](https://code.google.com/p/cssmin/) (maintained via [composer](http://getcomposer.org/)) to minify the css.
+
+## Usage
+Requires PHP >= 5.4 (earlier versions untested)
+Requires Java (for Closure)
+
+### Command Line Format
+Specify any options and then specify the files or directories that you'd like the minifier to work through.
+
+`php minify.php (options) (file1 (file2 (...)))`
+
+### Example
+`php minify.php -r static/css/ static/js/`
+
+### Command Line Options
+- `-r` : Recurse through any sub-directories that may be present.
+
+### License
+
+The Closure Compilier is liscensed under [Apache 2](http://www.apache.org/licenses/LICENSE-2.0)
+Everything else is liscensed under [MIT](http://opensource.org/licenses/MIT)
